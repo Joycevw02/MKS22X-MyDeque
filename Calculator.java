319b1978eff9bc775a3e7df36d161df2cb3e041d
joycevw02 <jwang01@stuy.edu>
2019-03-25 14:53:45 -0400

    Testing around with some stuff

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,25 @@
+import java.util.*;
+import java.io.*;
 public class Calculator{
     /*Evaluate a postfix expression stored in s.
      *Assume valid postfix notation, separated by spaces.
      */
-    public static double eval(String s){}
+//    public static double eval(String s){
+//    }
+
+    public static void main(String[] args){
+      String test = "1 23 4 56 7 + * - -";
+      Queue<String> testQ = new LinkedList<>();
+      String temp = "";
+      for (int i = 0; i < test.length(); i ++){
+        if (test.charAt(i) != ' '){
+          temp += test.substring(i, i + 1);
+        }
+        else{
+          testQ.add(temp);
+          temp = "";
+        }
+      }
+      System.out.println(testQ);
+    }
 }
